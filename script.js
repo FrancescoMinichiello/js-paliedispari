110fba38678d63fe2c438eb8a41b7fd7f8a47437
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-22 17:41:30 +0200

    Esercizio finito

--- script.js
+++ script.js
@@ -3,25 +3,35 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+//1- Chiedere all'utente una parola
 //2- creare una funzione
 //3- stampare in console
+/*
+function palindroma(parola) {
+    const splitWord = parola.split('');
+
+    //Inverto la parola
+    let result = []
+    for (let i = splitWord.length - 1; i >= 0; i--) {
+        result.push(splitWord[i])
+    }
+    const reversedWord = result.join('');
+    let risultato = parola === reversedWord ? true : false
+    return risultato
+}
+
 
 //!Fase di preparazione
-//1- Chiedere all'utente una parola
-const word = prompt('Inserisci una parola', 'osso');
-console.log(word);
-//divido la parola
-const splitWord = word.split('');
-console.log(splitWord);
-//Inverto la parola
-const reversedWord = splitWord.join('');
-console.log(reversedWord);
+const word = prompt('Inserisci una parola', 'cane');
 
-if (word === reversedWord) {
+//divido la parola
+
+if (palindroma(word)) {
     console.log('La parola è palindroma')
 } else {
     console.log('La parola non è palindroma')
 }
+*/
 
 
 
@@ -43,12 +53,7 @@ if (word === reversedWord) {
 
 
 
-
-
-
-
-/*
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
@@ -60,3 +65,37 @@ Consigli del giorno
 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 Buon lavoro e buon divertimento!
 */
+
+
+//1 chiedere all'utente un numero
+//2 generare un numero random da 1 a 5 per il computer usando una funzione
+
+//Fase di preparazione
+//creo la funzione per generare un numero
+function randomCpuNumber() {
+    let min = 1;
+    let max = 5;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isEven(number) {
+    if (number % 2 === 0) {
+        console.log('il numero è pari')
+    } else {
+        console.log('il numero è dispari')
+    }
+}
+const cpuNumber = randomCpuNumber();
+console.log(`Il numero casuale generato è: ${cpuNumber}`);
+
+let playerNumber = parseInt(prompt('dimmi un numero da 1 a 5', parseInt(4)))
+console.log(`Il numero che hai scelto è: ${playerNumber}`);
+
+let sum = cpuNumber + playerNumber;
+console.log(`La somma dei numeri è: ${sum}`)
+
+isEven(sum);
+
+
+
+
